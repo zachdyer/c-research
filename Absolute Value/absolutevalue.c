a22ea687375f6ed4639a81cae0e43dc5b87a317e
Zach Dyer <zach.dyer@zachdyerdesign.com>
2013-03-23 17:30:53 -0500

    Testing how to return a char from array

---
+++ Absolute Value/absolutevalue.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main(int argc, char * argv[]){
+	//puts(“Hello world”); 
+	char ben[] = "Ben";
+	char zach[] = {'Z','a','c','h','\0'};
+	//char arr[] = {'c','o','d','e','\0'};
+
+	printf("Hello, %s \n", ben);
+	printf("Hello, %s \n", zach);
+	printf("Hello, %c \n", zach[0]);
+}
