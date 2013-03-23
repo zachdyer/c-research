17caeb2ff81ea6e6ede2311ff33aab856ad41e2b
zachdyer <zach.dyer@zachdyerdesign.com>
2013-03-23 17:33:19 -0500

    Adding hello world

--- Absolute Value/absolutevalue.c
+++ Absolute Value/absolutevalue.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 main(int argc, char * argv[]){
-	//puts(“Hello world”); 
+	//puts(ï¿½Hello worldï¿½); 
 	char ben[] = "Ben";
 	char zach[] = {'Z','a','c','h','\0'};
 	//char arr[] = {'c','o','d','e','\0'};
@@ -9,4 +9,5 @@ main(int argc, char * argv[]){
 	printf("Hello, %s \n", ben);
 	printf("Hello, %s \n", zach);
 	printf("Hello, %c \n", zach[0]);
+	printf("Hello World!");
 }
