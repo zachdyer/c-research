58536259d2b7da2828c1a547af3f404b06f3f2e8
Zach Dyer <zach.dyer@zachdyerdesign.com>
2013-03-23 15:21:36 -0500

    Primary Numbers advanced version
    primenumbers <max>

--- Prime Numbers/primenumbers.c
+++ Prime Numbers/primenumbers.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
 #include <stdlib.h> 
 
-main(int argc, char * argv[]) {
-	//A prime number is a natural number divisible by 2 natural numbers.
-	printf("Find the prime numbers between 1 and any number\n");
-	printf("Enter the maximum number: \n");
-	char max[16];
-	gets(max);
+void prime(int max){
 	int i;
-	for(i = 1; i <= strtol(max,NULL,0); i++){
-		//printf("%d\n",i);
+	for(i = 1; i <= max; i++){
 		int j;
 		int divisors = 0;
 		for(j = 1; j <= i; j++){
@@ -21,5 +15,23 @@ main(int argc, char * argv[]) {
 		if(divisors == 2){
 			printf("%d is prime\n",i);
 		}
+	}
+}
+
+void main(int argc, char * argv[]) {
+	//A prime number is a natural number divisible by 2 natural numbers.
+	
+	if(argc < 2){
+		printf("Find the prime numbers between 1 and any number\n");
+		printf("Enter the maximum number: \n");
+		char maxChar[16];
+		gets(maxChar);
+		int max = strtol(maxChar,NULL,0);
+		prime(max);
+		
+	} else {
+		int max = strtol(argv[1],NULL,0);
+		prime(max);
+		printf("%s",argv[1]);
 	}
 }	
